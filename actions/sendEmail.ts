62b59430a8ad12cb604913a79427ded99d9112e9
FotsoPliya <dfotso2015@gmail.com>
2024-08-14 00:12:54 +0100

    email sending succesfully implemented

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,11 +1,11 @@
-"use clent";
+"use server";
 
 import React from "react";
 import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@lib/utils";
 import ContactFormEmail from "@email/contact-form-email";
 
-const resend = new Resend('re_ShL2nz2J_2oRq47yfiFj27BBeqvPjbKaF');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
@@ -28,7 +28,7 @@ export const sendEmail = async (formData: FormData) => {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
       to: "dfotso2015@gmail.com",
-      subject: "Message from contact form",
+      subject: "Message from AlphaCorp's website contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
         message: message,
