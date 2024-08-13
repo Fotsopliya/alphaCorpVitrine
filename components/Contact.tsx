5ca0f6f2f6437814b8fa34d7d26081e38de4dbe2
FotsoPliya <dfotso2015@gmail.com>
2024-08-13 23:42:58 +0100

    removed unecessary codes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,18 +2,14 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import { useSectionInView } from "@/lib/hooks";
 import { sendEmail } from "@actions/sendEmail";
 import SubmitBtn from "./ui/submit-btn";
 import toast from "react-hot-toast";
 
 const Contact = () => {
-    const { ref } = useSectionInView("Contact");
 
   return (
     <motion.section
-      id="contact"
-      ref={ref}
       className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
       initial={{
         opacity: 0,
