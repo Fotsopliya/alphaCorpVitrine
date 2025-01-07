5ad6e654b6eaceda9a3dfc0244abf521758c9775
FotsoPliya <dfotso2015@gmail.com>
2025-01-07 19:13:15 +0100

    fixing build errors

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,27 +1,26 @@
-'use client';
 
 import Footer from '@components/Footer';
 import Navbar from '@components/Navbar';
 import Loader from '@components/Loader';
 import '@styles/globals.css';
 import { Toaster } from 'react-hot-toast';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const [loading, setLoading] = useState(true);
-  const MINIMUM_LOADING_TIME = 500; // 500ms minimum
+  // const [loading, setLoading] = useState(true);
+  // const MINIMUM_LOADING_TIME = 500; // 500ms minimum
 
-  useEffect(() => {
-    const handleInitialLoad = () => {
-      setTimeout(() => setLoading(false), MINIMUM_LOADING_TIME); // Ensure loader is visible for at least 500ms
-    };
+  // useEffect(() => {
+  //   const handleInitialLoad = () => {
+  //     setTimeout(() => setLoading(false), MINIMUM_LOADING_TIME); // Ensure loader is visible for at least 500ms
+  //   };
 
-    // Simulate initial page load by waiting for a moment (or you could check if children are rendered)
-    handleInitialLoad();
+  //   // Simulate initial page load by waiting for a moment (or you could check if children are rendered)
+  //   handleInitialLoad();
 
-    // Optional: Cleanup function in case of dependencies (here, no event listeners are used)
-    return () => {};
-  }, []); // Empty dependency array means this runs only once on mount (page load)
+  //   // Optional: Cleanup function in case of dependencies (here, no event listeners are used)
+  //   return () => {};
+  // }, []); // Empty dependency array means this runs only once on mount (page load)
 
   return (
     <>
@@ -30,7 +29,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </div>
       <main className="app">
         <Navbar />
-        {loading ? <Loader /> : children}
+        {/* {loading ? <Loader /> : children} */}
+        {children}
         <Footer />
         <Toaster position="top-center" />
       </main>
