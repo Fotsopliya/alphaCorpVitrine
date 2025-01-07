7b38d9ad6e7e57f657ea517cac8e4054c122b553
FotsoPliya <dfotso2015@gmail.com>
2025-01-07 20:41:16 +0100

    added enviromental variables for database

--- app/(root)/news/page.tsx
+++ app/(root)/news/page.tsx
@@ -1,12 +1,15 @@
-
+"use client"
 import React from 'react'
 import BlogCard from "@components/ui/BlogCard";
+import { useTranslation } from 'react-i18next';
 
 const NewsPage = () => {
+  const { t } = useTranslation('common'); 
+
     return (
         <div className='mx-[5%]'>
             <h1 className='text-4xl font-bold p-10 text-gray-600'>Newsroom</h1>
-            <h2 className='text-2xl font-bold p-10 text-gray-600'>À LA UNE</h2>
+            <h2 className='text-2xl font-bold p-10 text-gray-600'>{t("A LA UNE")}</h2>
             <div>
                 <BlogCard/>
             </div>
